ca962d0e00c50cce0e0e67233da3fbeb93f68ece
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2026-09-02 21:41:23 +0900

    CORS変更

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://shift-schedular-one.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
