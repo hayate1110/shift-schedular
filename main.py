79fae67c7284082d9a5a69e00f1fbfc1313b5d3d
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2026-09-02 20:07:44 +0900

    frontend追加

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 from scheduler import solve_schedule
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ScheduleRequest(BaseModel):
